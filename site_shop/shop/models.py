62f76a96e9105dc4ec02533f3c26466b7f755789
Vyfme <yaskpenta248@gmail.com>
2025-11-16 01:01:54 +0300

    Update models and Deleted the SQLite database file

--- site_shop/shop/models.py
+++ site_shop/shop/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
 class Goods(models.Model):
     name = models.CharField(max_length=150)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    percent = models.IntegerField()
-    in_stock = models.BooleanField()
-    pub_date = models.DateTimeField()
+    discount = models.IntegerField()
+    in_stock = models.PositiveIntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
