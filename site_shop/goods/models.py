744e2523b8d08b5083c4123b848e78a83cf64998
Vyfme <yaskpenta248@gmail.com>
2025-11-28 02:38:04 +0300

    Register simple tag for categories

--- site_shop/goods/models.py
+++ site_shop/goods/models.py
@@ -18,7 +18,7 @@ class Goods(models.Model):
     name = models.CharField(max_length=200, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='goods/images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Скидка')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
@@ -32,3 +32,9 @@ class Goods(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+        
+        return self.price
--- site_shop/shop/views.py
+++ site_shop/shop/views.py
@@ -2,22 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-from goods.models import Categories
-
-categories = Categories.objects.all()
-
-
 def index(request):
-    return render(request, "shop/index.html", context={'title': 'Главная страница', 'categories': categories})
+    return render(request, "shop/index.html", context={'title': 'Главная страница'})
 
 
 def about(request):
-
-    return render(request, "shop/about.html", context={'title': 'О сайте', 'categories': categories})
+    return render(request, "shop/about.html", context={'title': 'О сайте'})
 
 
 def contact(request):
-    return render(request, "shop/contact.html", context={'title': 'Контакты', 'categories': categories})
+    return render(request, "shop/contact.html", context={'title': 'Контакты'})
 
 
 def basket(request):
